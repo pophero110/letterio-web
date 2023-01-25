70cdab9e70b9c40a2c61f25c8c4772304081ed14
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-24 22:49:50 -0600

    add form api

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -1,10 +1,11 @@
-import { db } from '../../utils/firebase';
+import { app } from '../../utils/firebase';
 import { ref, set } from 'firebase/database';
 export default async function Form(req, res) {
-	await set(ref(db, 'users/' + 12), {
+	const db = getDatabase(app);
+	await set(ref(db, 'form/' + 12), {
 		username: 'name',
 		email: 'email',
 		profile_picture: 'imageUrl',
 	});
-	res.status(200).json('form');
+	res.status(200).json('testing');
 }
--- src/pages/api/generateLetter.js
+++ src/pages/api/generateLetter.js
@@ -3,44 +3,35 @@ import { app } from '../../utils/firebase';
 import { getDatabase, ref, set } from 'firebase/database';
 export default async function generateLetter(req, res) {
 	if (req.method === 'POST') {
-		// const body = JSON.parse(req.body);
-		// let url = `${
-		// 	process.env.CHATGPT_API
-		// }/sendMessage?fields=${JSON.stringify(
-		// 	body.fields
-		// )}&letterType=${JSON.stringify(body.letterType)}`;
-
-		// const { conversationId, messageId } = parseCookies({ req });
-		// if (conversationId && messageId) {
-		// 	url += `&conversationId=${conversationId}`;
-		// 	url += `&messageId=${messageId}`;
-		// }
-		// console.log();
-		// const response = await fetch(url, {
-		// 	method: 'GET',
-		// });
-
-		// response { response, conversationId, messageID}
-		// const data = await response.json();
-		// if (data.response) {
-		// 	setCookie({ res }, 'conversationId', data.conversationId, {
-		// 		maxAge: 30 * 60,
-		// 	});
-		// 	setCookie({ res }, 'messageId', data.messageId, {
-		// 		maxAge: 30 * 60,
-		// 	});
-		// 	res.status(200).json(data.response);
-		// } else {
-		// 	res.status(400).json(
-		// 		'Something went wrong. Please try again later'
-		// 	);
-		// }
-		const db = getDatabase(app);
-		await set(ref(db, 'form/' + 12), {
-			username: 'name',
-			email: 'email',
-			profile_picture: 'imageUrl',
+		const body = JSON.parse(req.body);
+		let url = `${
+			process.env.CHATGPT_API
+		}/sendMessage?fields=${JSON.stringify(
+			body.fields
+		)}&letterType=${JSON.stringify(body.letterType)}`;
+		const { conversationId, messageId } = parseCookies({ req });
+		if (conversationId && messageId) {
+			url += `&conversationId=${conversationId}`;
+			url += `&messageId=${messageId}`;
+		}
+		console.log();
+		const response = await fetch(url, {
+			method: 'GET',
 		});
-		res.status(200).json('testing');
+		// response { response, conversationId, messageID}
+		const data = await response.json();
+		if (data.response) {
+			setCookie({ res }, 'conversationId', data.conversationId, {
+				maxAge: 30 * 60,
+			});
+			setCookie({ res }, 'messageId', data.messageId, {
+				maxAge: 30 * 60,
+			});
+			res.status(200).json(data.response);
+		} else {
+			res.status(400).json(
+				'Something went wrong. Please try again later'
+			);
+		}
 	}
 }
