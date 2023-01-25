b25c7c09e848bff5622728f6ddb02adc837c9de6
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-25 13:21:27 -0600

    remove firebase

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -1,11 +1,3 @@
-import { app } from '../../utils/firebase';
-import { ref, set } from 'firebase/database';
 export default async function Form(req, res) {
-	const db = getDatabase(app);
-	await set(ref(db, 'form/' + 12), {
-		username: 'name',
-		email: 'email',
-		profile_picture: 'imageUrl',
-	});
 	res.status(200).json('testing');
 }
