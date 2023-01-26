71c0e70e7e2af74d848ee5f649b222b39c3b438f
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-25 21:32:35 -0600

    avoid max 10 execution timeout

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -1,3 +1,14 @@
+import { unstable_getServerSession } from 'next-auth/next';
+import { authOptions } from './auth/[...nextauth]';
+import { prisma } from '../../server/db';
 export default async function Form(req, res) {
-	res.status(200).json('testing');
+	const session = await unstable_getServerSession(req, res, authOptions);
+	console.log(session);
+	const { id } = req.query;
+	const letter = await prisma.letter.findUnique({
+		where: {
+			id,
+		},
+	});
+	res.status(200).json(letter);
 }
--- src/pages/api/generateLetter.js
+++ src/pages/api/generateLetter.js
@@ -1,4 +1,7 @@
 import { setCookie, parseCookies } from 'nookies';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
+import { prisma } from '../../server/db';
 export default async function generateLetter(req, res) {
 	if (req.method === 'POST') {
 		const body = JSON.parse(req.body);
@@ -12,24 +15,47 @@ export default async function generateLetter(req, res) {
 			url += `&conversationId=${conversationId}`;
 			url += `&messageId=${messageId}`;
 		}
-		console.log();
-		const response = await fetch(url, {
+
+		const session = await unstable_getServerSession(req, res, authOptions);
+		console.log(session);
+
+		const response = fetch(url, {
 			method: 'GET',
 		});
+
+		const letter = await prisma.letter.create({
+			data: {
+				content: '',
+			},
+		});
+
+		response.then(async (res) => {
+			const data = await res.json();
+			console.log(data.response);
+			await prisma.letter.update({
+				where: { id: letter.id },
+				data: {
+					content: data.response,
+				},
+			});
+		});
+
 		// response { response, conversationId, messageID}
-		const data = await response.json();
-		if (data.response) {
-			setCookie({ res }, 'conversationId', data.conversationId, {
-				maxAge: 30 * 60,
-			});
-			setCookie({ res }, 'messageId', data.messageId, {
-				maxAge: 30 * 60,
-			});
-			res.status(200).json(data.response);
-		} else {
-			res.status(400).json(
-				'Something went wrong. Please try again later'
-			);
-		}
+		// const data = await response.json();
+		// if (data.response) {
+		// 	setCookie({ res }, 'conversationId', data.conversationId, {
+		// 		maxAge: 30 * 60,
+		// 	});
+		// 	setCookie({ res }, 'messageId', data.messageId, {
+		// 		maxAge: 30 * 60,
+		// 	});
+		// 	res.status(200).json(data.response);
+		// } else {
+		// 	res.status(400).json(
+		// 		'Something went wrong. Please try again later'
+		// 	);
+		// }
+
+		res.status(200).json({ message: 'Generating', letterId: letter.id });
 	}
 }
