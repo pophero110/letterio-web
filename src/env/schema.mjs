e27d4470ae7c47383538322ddce8381b9b94aab9
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-26 23:59:38 -0600

    add new env vars

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -1,26 +1,28 @@
 // @ts-check
-import { z } from "zod";
+import { z } from 'zod';
 
 /**
  * Specify your server-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET:
-    process.env.NODE_ENV === "production"
-      ? z.string().min(1)
-      : z.string().min(1).optional(),
-  NEXTAUTH_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+	DATABASE_URL: z.string().url(),
+	NODE_ENV: z.enum(['development', 'test', 'production']),
+	NEXTAUTH_SECRET:
+		process.env.NODE_ENV === 'production'
+			? z.string().min(1)
+			: z.string().min(1).optional(),
+	NEXTAUTH_URL: z.preprocess(
+		// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+		// Since NextAuth.js automatically uses the VERCEL_URL if present.
+		(str) => process.env.VERCEL_URL ?? str,
+		// VERCEL_URL doesn't include `https` so it cant be validated as a URL
+		process.env.VERCEL ? z.string() : z.string().url()
+	),
+	DISCORD_CLIENT_ID: z.string(),
+	DISCORD_CLIENT_SECRET: z.string(),
+	GOOGLE_CLIENT_ID: z.string(),
+	GOOGLE_CLIENT_SECRET: z.string(),
 });
 
 /**
@@ -29,12 +31,14 @@ export const serverSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
  */
 export const serverEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  NODE_ENV: process.env.NODE_ENV,
-  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+	DATABASE_URL: process.env.DATABASE_URL,
+	NODE_ENV: process.env.NODE_ENV,
+	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+	NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+	DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+	DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 };
 
 /**
@@ -43,7 +47,7 @@ export const serverEnv = {
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+	// NEXT_PUBLIC_CLIENTVAR: z.string(),
 });
 
 /**
@@ -53,5 +57,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+	// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
